46f305041a35f73431eca8198cfc09b0f92a37a0
TaoXuSheng <xusheng.tao@ele.me>
2018-04-25 10:57:56 +0800

    基础框架搭建完成

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  title: 'Hello MT-BLOG',
+  description: '12345，上山打老虎。',
+  themeConfig: {
+    repo: 'TaoXuSheng/vuepress-blog',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Blog', link: '/blog/' },
+      { text: '知乎', link: '/zhihu/' },
+      {
+        text: '我的项目',
+        items: [
+          { text: 'focus-outside', link: 'https://github.com/TaoXuSheng/focus-outside' },
+          { text: 'stylus-converter', link: 'https://github.com/TaoXuSheng/stylus-converter' },
+        ]
+      }
+    ],
+    sidebar: {
+      '/blog/': [
+        '',
+      ],
+      '/zhihu/': [
+        ''
+      ]
+    }
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,11 @@
+---
+home: true
+features:
+- title: 我的项目
+  details: 在 github 中开源的项目。
+- title: 知乎
+  details: 在知乎专栏中发表过的文章。
+- title: BLOG
+  details: 在日常中学习和总结的一些经验。
+footer: MIT Licensed | Copyright © 2018-present TaoXuSheng
+---
