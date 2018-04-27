daa47d2bcf6590601dca630ac40e4562a5581a8c
TaoXuSheng <xusheng.tao@ele.me>
2018-04-27 15:28:47 +0800

    Updates

---
+++ assets/js/8.48d137c4.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{180:function(t,e,n){"use strict";n.r(e);var l={},s=n(1),u=Object(s.a)(l,function(){var t=this.$createElement;return(this._self._c||t)("div",[this._v("default")])},[],!1,null,null,null);e.default=u.exports}}]);
