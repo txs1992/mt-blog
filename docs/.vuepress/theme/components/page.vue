4a8676cd2cb43ce4400bc3be457f11a355f85462
TaoXuSheng <xusheng.tao@ele.me>
2018-05-11 17:17:44 +0800

    add page

--- docs/.vuepress/theme/components/page.vue
+++ docs/.vuepress/theme/components/page.vue
@@ -1,24 +1,34 @@
 <template>
   <div class="component-page">
-    <home></home>
+    <home v-if="$route.path === '/'"></home>
+    <div
+      v-else
+      class="page-content">
+      <item-list>
+      </item-list>
+      <Content/>
+    </div>
   </div>
 </template>
 
 <script>
 import Home from './Home.vue'
+import ItemList from './item-list.vue'
+import 'prismjs/themes/prism-tomorrow.css'
+import 'prismjs/prism.js'
 
 export default {
   watch: {
     '$route': {
       immediate: true,
       handler (v) {
-        console.log(this.$page.frontmatter)
       }
     }
   },
 
   components: {
-    Home
+    Home,
+    ItemList
   }
 }
 </script>
@@ -27,5 +37,13 @@ export default {
 .component-page {
   width: 100%;
   height: 100%;
+
+  .page-content {
+    min-width: 800px;
+    width: 60%;
+    height: 100%;
+    margin: auto;
+    overflow: auto;
+  }
 }
 </style>
